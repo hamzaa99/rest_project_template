1a11ec6ee8feb683e4c0d67418cf1bc95d847dd9
hamzaa99 <61057052+hamzaa99@users.noreply.github.com>
2021-02-17 23:16:50 +0100

    Initial commit

---
+++ src/rest/todo/model/Article.java
@@ -0,0 +1,51 @@
+package rest.todo.model;
+
+public class Article {
+	
+	private String libelle;
+	private String marque;
+	private Double prix;
+	private Categorie categorie;
+	private String photo; //URL de la photo
+	
+	Article(String libelle,String marque,Double prix,Categorie categorie,String photo){
+		this.libelle=libelle;
+		this.marque=marque;
+		this.prix=prix;
+		this.categorie=categorie;
+		this.photo=photo;
+	}
+	
+	
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	public String getMarque() {
+		return marque;
+	}
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+	public Double getPrix() {
+		return prix;
+	}
+	public void setPrix(Double prix) {
+		this.prix = prix;
+	}
+	public Categorie getCategorie() {
+		return categorie;
+	}
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+}
---
+++ src/rest/todo/model/Categorie.java
@@ -0,0 +1,6 @@
+package rest.todo.model;
+
+public enum Categorie {
+	PC_Portable,PC_Bureau,Accessoire_Ordinateur,SmartPhone,Tel_Fixe,Accessoire_Telephone,Disque_dur,Cle_USB,Accessoire_Stockage
+
+}
